15e9f749bae82df80edfc5c2376c8965097f73f6
PogorenkoIvan <145456386+PogorenkoIvan@users.noreply.github.com>
2023-10-21 23:16:49 +0700

    Add files via upload

---
+++ lab5/index.js
@@ -0,0 +1,16 @@
+function openModal (targetModelId) {
+    $("#" + targetModelId).fadeIn(400);
+    $(".overlay").fadeIn(400);
+}
+
+function closeModal (targetModelId) {
+    $("#" + targetModelId).fadeOut(400);
+    $(".overlay").fadeOut(400);
+}
+
+const buttons = document.querySelectorAll(".btnOpen");
+buttons.forEach(element => {
+    const targetModelId = element.getAttribute("target-model-id");
+    element.addEventListener("click", () => openModal(targetModelId));
+    document.querySelector(".overlay").addEventListener("click", () => closeModal(targetModelId));
+});
